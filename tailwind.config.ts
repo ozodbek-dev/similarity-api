ef182e102e354032bd174e96c15e02fecb48532a
ozodbek-dev <ozodbeksoftwaredev@gmail.com>
2023-09-05 20:50:34 +0500

    Landing page have been added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from 'tailwindcss'
+const { colors } = require("tailwindcss/colors");
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: ["class"],
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+
+		// Or if using `src` directory:
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		container: {
+			center: true,
+			padding: "1.5rem",
+			screens: {
+				"2xl": "1360px",
+			},
+		},
+		extend: {
+			fontFamily: {
+				sans: ["var(--font-inter)", ...fontFamily.sans],
+			},
+			colors: {
+				...colors,
+				"light-gold": "#f5bc51",
+				"dark-gold": "#533519",
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+};
